e38214a8c771831d3e270bf7ea09989722a079be
cySheng <cyusheng93@gmail.com>
2018-03-26 15:42:18 +0800

    - Added jquery gem for additional ajax feature - Added readme explaining how to get each ajax feature to work - Created two methods for performing ajax request, the jquery ajax way and the rails way

--- app/controllers/generic_controller.rb
+++ app/controllers/generic_controller.rb
@@ -3,7 +3,10 @@ class GenericController < ApplicationController
   end
 
   def search
-    cities = Location.search_city(params["query"])
-    render json: cities
+    @cities = Location.search_city(params["query"])
+    respond_to do |format|
+      format.json { render json: @cities }
+      format.js # remote: true is sent a js format and sends you to search.js.erb
+    end
   end
 end
